f23709f7d887cc6411938e3fa69052ae986894a4
Mark Seliverstov <smdmrr00@gmail.com>
2024-01-05 10:53:20 +0100

    Add keybindings for MD preview

---
+++ after/plugin/markdowndPreview.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>mp", vim.cmd.MarkdownPreview)
+vim.keymap.set("n", "<leader>mpt", vim.cmd.MarkdownPreviewToggle)
+vim.keymap.set("n", "<leader>mps", vim.cmd.MarkdownPreviewStop)
