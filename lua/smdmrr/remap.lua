80cd2309d7f4533210ab9499f1a77e867ab695e4
Mark Seliverstov <smdmrr00@gmail.com>
2024-01-16 14:57:43 +0100

    Clean up keymaps

--- lua/smdmrr/remap.lua
+++ lua/smdmrr/remap.lua
@@ -10,18 +10,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -33,29 +25,21 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Make file exectable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("i", "fj", "<esc>")           -- fast escape
-vim.keymap.set("n", "<leader>,", ":noh<CR>") -- disable highligths
 
 -- just comfo
+vim.keymap.set("n", "<leader>,", ":noh<CR>") -- disable highligths
 vim.keymap.set("n", "L", "$")
 vim.keymap.set("n", "H", "^")
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<leader>q", ":q!<Cr>")
 vim.keymap.set("n", "<leader>a", ":wa<Cr>")
+vim.keymap.set("i", "fj", "<esc>")           -- fast escape
 
 -- don't use arrows keys
 vim.keymap.set("", "<Up>", "<nop>")
 vim.keymap.set("", "<Down>", "<nop>")
 vim.keymap.set("", "<Left>", "<nop>")
 vim.keymap.set("", "<Right>", "<nop>")
-
--- Breckets autocomplitions
--- vim.keymap.set("i", "(", "()<Esc>i")
--- vim.keymap.set("i", "{", "{}<Esc>i")
--- vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>O")
--- vim.keymap.set("i", "[", "[]<Esc>i")
--- vim.keymap.set("i", "'", "''<Esc>i")
--- vim.keymap.set("i", "\"", "\"\"<Esc>i")
 
 -- Window managment
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
