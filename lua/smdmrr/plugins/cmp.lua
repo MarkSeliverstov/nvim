6fb58b4c1f594e5904b62dd3ee6c5b38a9886095
Mark Seliverstov <smdmrr00@gmail.com>
2024-01-28 23:48:00 +0100

    Separate plugins

---
+++ lua/smdmrr/plugins/cmp.lua
@@ -0,0 +1,6 @@
+return {
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-path" },
+  { "L3MON4D3/LuaSnip" },
+}
---
+++ lua/smdmrr/plugins/lsp.lua
@@ -0,0 +1,6 @@
+return {
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "VonHeikemen/lsp-zero.nvim" },
+}
