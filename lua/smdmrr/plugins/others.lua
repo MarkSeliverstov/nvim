dba9649a3f5f3768b4db2b1f4c58710c6ffade7c
Mark Seliverstov <smdmrr00@gmail.com>
2024-02-10 18:02:41 +0100

    Add others plugins

---
+++ lua/smdmrr/plugins/others.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "godlygeek/tabular",
+        keys = {
+            { "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>"},
+        },
+    },
+    "eandrju/cellular-automaton.nvim",
+    "junegunn/goyo.vim",
+    "github/copilot.vim",
+}
+
