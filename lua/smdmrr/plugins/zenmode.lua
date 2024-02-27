1dd9433c83a6f143b9bf8126624e8fe8c241aae3
Mark Seliverstov <smdmrr00@gmail.com>
2024-02-27 23:05:53 +0100

    Upgrade zen mode

--- lua/smdmrr/plugins/zenmode.lua
+++ lua/smdmrr/plugins/zenmode.lua
@@ -5,29 +5,40 @@ return {
             require("zen-mode").setup {
                 window = {
                     width = 90,
-                    options = { }
                 },
             }
             require("zen-mode").toggle()
-            vim.wo.wrap = false
-            vim.wo.number = true
-            vim.wo.rnu = true
-            vim.opt.colorcolumn = "80"
         end)
 
 
         vim.keymap.set("n", "<leader>zZ", function()
             require("zen-mode").setup {
                 window = {
-                    width = 80,
-                    options = { }
+                    width = 90,
+                    options = {
+                        signcolumn = "no", -- disable signcolumn
+                        number = false,   -- disable number column
+                        relativenumber = false, -- disable relative numbers
+                        cursorcolumn = false, -- disable cursor column
+                        list = false,     -- disable whitespace characters
+                        colorcolumn = "0", -- disable color column
+                    },
                 },
+                -- callback where you can add custom code when the Zen window opens
+                on_open = function()
+                    -- if file is markdown - change colorscheme to one that is better for markdown
+                    if vim.bo.filetype == "markdown" then
+                        vim.cmd("colorscheme catppuccin")
+                        vim.cmd("PencilToggle")
+                    end
+                end,
+                -- callback where you can add custom code when the Zen window closes
+                on_close = function()
+                    vim.cmd("colorscheme kanagawa")
+                    vim.cmd("PencilToggle")
+                end,
             }
             require("zen-mode").toggle()
-            vim.wo.wrap = false
-            vim.wo.number = false
-            vim.wo.rnu = false
-            vim.opt.colorcolumn = "0"
         end)
     end
 }
