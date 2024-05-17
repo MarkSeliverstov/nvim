14b2ae430a94900f5b5b87f836a3875f0e6c40e7
Mark Seliverstov <smdmrr00@gmail.com>
2024-05-17 18:52:15 +0200

    Add autoformatting on enter buffer

--- lua/smdmrr/plugins/lsp.lua
+++ lua/smdmrr/plugins/lsp.lua
@@ -12,6 +12,15 @@ return {
 		config = function()
 			require("smdmrr.plugins.configs.lsp")
 			require("smdmrr.plugins.configs.formatting")
+
+			-- Autoformatting on Read
+			local foramtting_group = vim.api.nvim_create_augroup("formatting", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter" }, {
+				group = foramtting_group,
+				callback = function()
+					require("conform").format()
+				end,
+			})
 		end,
 	},
 }
