6dedf63c30bdd2de6a641262f8c7b0351e18ac07
Mark Seliverstov <smdmrr00@gmail.com>
2024-07-09 22:47:30 +0200

    chore: update formmating

--- lua/smdmrr/plugins/configs/formatting.lua
+++ lua/smdmrr/plugins/configs/formatting.lua
@@ -9,10 +9,30 @@ require("conform").setup({
 		typescript = { { "prettierd", "prettier" } },
 		javascript = { { "prettierd", "prettier" } },
 		graphql = { { "prettierd", "prettier" } },
+		json = { { "prettierd", "prettier" } },
+		yaml = { { "prettierd", "prettier" } },
+    vue = { { "prettierd", "prettier" } },
 	},
-	format_on_save = {
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 500,
-	},
+	-- format_on_save = {
+	-- 	lsp_fallback = true,
+	-- 	async = false,
+	-- 	timeout_ms = 500,
+	-- },
 })
+
+-- Autoformatting on Read
+-- local foramtting_group = vim.api.nvim_create_augroup("formatting", { clear = true })
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+-- 	group = foramtting_group,
+-- 	callback = function()
+-- 		require("conform").format()
+-- 	end,
+-- })
+
+-- Command to format the current buffer
+vim.api.nvim_set_keymap(
+	"n",
+	"FF",
+	"<cmd>lua require('conform').format()<CR>",
+	{ noremap = true, silent = true }
+)
