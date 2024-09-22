6d5baf282668e2f755863e2d739dacd906f5357a
Mark Seliverstov <smdmrr00@gmail.com>
2024-09-22 13:27:27 +0200

    chore: delete unused bindings

--- lua/smdmrr/plugins/markdown-preview.lua
+++ lua/smdmrr/plugins/markdown-preview.lua
@@ -5,8 +5,6 @@ return {
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
 		vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreview<Cr>", { silent = true, noremap = true })
-		vim.api.nvim_set_keymap("n", "<leader>mpt", ":MarkdownPreviewToggle<Cr>", { silent = true, noremap = true })
-		vim.api.nvim_set_keymap("n", "<leader>mps", ":MarkdownPreviewStop<Cr>", { silent = true, noremap = true })
 	end,
 	ft = { "markdown" },
 }
